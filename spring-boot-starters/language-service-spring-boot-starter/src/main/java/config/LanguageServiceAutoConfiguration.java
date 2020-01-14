6ee2f4dd073c6599e5a354943a51dde429e87f1f
Kedar Joshi <kdar_joshi@yahoo.com>
2020-01-15 00:42:03 +0530

    Added Spring Boot starters

---
+++ spring-boot-starters/language-service-spring-boot-starter/src/main/java/config/LanguageServiceAutoConfiguration.java
@@ -0,0 +1,22 @@
+package config;
+
+import com.locationguru.csf.language.LanguageService;
+import com.locationguru.csf.language.NoOpLanguageService;
+import org.springframework.boot.autoconfigure.condition.*;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnClass({ LanguageService.class })
+@ConditionalOnMissingBean(LanguageService.class)
+@EnableConfigurationProperties({ LanguageServiceProperties.class })
+public class LanguageServiceAutoConfiguration
+{
+	@Bean(initMethod = "initialize", destroyMethod = "close")
+	@ConditionalOnProperty(prefix = "application.language-service", name = "provider", havingValue = "no-op", matchIfMissing = true)
+	public NoOpLanguageService noOpLanguageService()
+	{
+		return new NoOpLanguageService();
+	}
+}
---
+++ spring-boot-starters/language-service-spring-boot-starter/src/main/java/config/LanguageServiceProperties.java
@@ -0,0 +1,10 @@
+package config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "application.language-service")
+public class LanguageServiceProperties
+{
+
+
+}
